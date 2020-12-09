569b19c3ea3a39f4e4ed160db690b12ce479579b
Omer Khalid Khan <omer.khalid@turbolabz.com>
2020-12-09 16:53:57 +0500

    Event raised example for statistics

---
+++ PhotonHooks/EventRaised.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using PlayFab.ServerModels;
+using PlayFab.Json;
+using System.Collections.Generic;
+using PlayFab.DataModels;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+// using PlayFab.Plugins.CloudScript;
+using PlayFab.Samples;
+using SocialEdge.Server.Constants;
+using PlayFab;
+using System.Text;
+using System.Net;
+using SocialEdge.Server.Util;
+namespace SocialEdge.Playfab
+{
+    public class RoomJoined
+    {
+//      EventRaised arguments
+//      EvCode: custom event code.
+//      Data: custom event data as sent from client SDK.
+//      State: a serialized snapshot of the room's full state. It's sent only if SendState webflag is set when calling OpRaiseEvent 
+//              and "IsPersistent" setting is set to true.
+//      AuthCookie: an encrypted object invisible to client, optionally returned by web service upon successful custom authentication. 
+//              It's sent only if SendAuthCookie webflag is set when calling OpRaiseEvent
+
+        [FunctionName("EventRaised")]
+        public async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "{appId}/RoomJoined/{id:int?}")] 
+            HttpRequest req, string appId)
+        {
+            var tasks = new List<Task<PlayFabResult<UpdatePlayerStatisticsResult>>>();
+            var context = await Util.Init(req);
+            dynamic args = context.FunctionArgument;
+            if(args!=null)
+            {
+                
+                var eventRaised = args["EvCode"];
+                var data = args["Data"];
+                if(eventRaised=="UpdStats")
+                {
+                    /*extract properties*/
+                    foreach(var player in data)
+                    {
+                        /*Example code which sets value to 1*/
+
+                        var request = new PlayFab.ServerModels.UpdatePlayerStatisticsRequest 
+                        {
+                            PlayFabId = player.Id,
+                            Statistics = new List<StatisticUpdate>
+                            {
+                                new StatisticUpdate{StatisticName="won", Value=1}
+                            }
+                        };
+                        var updateStatsTask = PlayFabServerAPI.UpdatePlayerStatisticsAsync(request);
+                        tasks.Add(updateStatsTask);
+                    }        
+
+                    var result = Task.WhenAll(tasks);
+                    if(tasks.FindAll(t=>t.IsCompletedSuccessfully).Count == tasks.Count)
+                    {
+                        /*all tasks have completed successfully*/
+                    }
+                }
+            }
+            
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
+
