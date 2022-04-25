a0ba58c447cac4cbcde4e0b951844108e771ffe5
Noor Khawaja <noor.khawaja@turbolabz.com>
2022-04-25 11:31:43 +0500

    Update Player Data Function

--- UpdatePlayerData.cs
+++ UpdatePlayerData.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using PlayFab.Samples;
 using System.Net;
 using SocialEdge.Server.Common.Utils;
+using System.Collections.Generic;
 
 namespace SocialEdge.Server.Requests
 {
@@ -19,10 +20,25 @@ namespace SocialEdge.Server.Requests
             ILogger log)
         {
             SocialEdgeEnvironment.Init(req);
-            var context = JsonConvert.DeserializeObject<PlayerPlayStreamFunctionExecutionContext<dynamic>>(await req.Content.ReadAsStringAsync());
+            var context = JsonConvert.DeserializeObject<FunctionExecutionContext<dynamic>>(await req.Content.ReadAsStringAsync());
             dynamic args = context.FunctionArgument;
+            string playerId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId;
+            var data = args["data"];
+
+            Dictionary<string, string> dataDict = new Dictionary<string, string>();
+            foreach (var dataItem in data)
+            {
+                dataDict.Add(dataItem.Name.ToString(), dataItem.Value.ToString());
+            }
+            
+            PlayFab.ServerModels.UpdateUserDataRequest getUserDataReq = new PlayFab.ServerModels.UpdateUserDataRequest();
+            getUserDataReq.PlayFabId = playerId;
+            getUserDataReq.Data = dataDict;
+
+            var getUserDataT = await PlayFab.PlayFabServerAPI.UpdateUserReadOnlyDataAsync(getUserDataReq);
+            var res = getUserDataT.Result;
             
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
-    }
+   }
 }
