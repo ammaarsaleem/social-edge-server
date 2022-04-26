c26e7a98fb89ff137f111e772e9fdc01033837ae
Noor Khawaja <noor.khawaja@turbolabz.com>
2022-04-26 07:15:37 +0500

    Update Public Data objects

--- UpdatePublicData.cs
+++ UpdatePublicData.cs
@@ -8,6 +8,9 @@ using PlayFab.Samples;
 using System.Net;
 using SocialEdge.Server.Common.Utils;
 using System.Collections.Generic;
+using PlayFab.DataModels;
+using SocialEdge.Server.Api;
+using PlayFab;
 
 namespace SocialEdge.Server.Requests
 {
@@ -24,18 +27,27 @@ namespace SocialEdge.Server.Requests
             string playerId = context.CallerEntityProfile.Lineage.MasterPlayerAccountId;
             var data = args["data"];
 
-            Dictionary<string, string> dataDict = new Dictionary<string, string>();
+            var getTitleTokenT = await Player.GetTitleEntityToken();
+
+            List<SetObject> dataList =  new List<SetObject>();
             foreach (var dataItem in data)
             {
-                dataDict.Add(dataItem.Name.ToString(), dataItem.Value.ToString());
+                SetObject obj = new SetObject();
+                obj.ObjectName = dataItem.Name.ToString();
+                obj.DataObject = dataItem.Value.ToString();
+                dataList.Add(obj);
             }
-            
-            PlayFab.ServerModels.UpdateUserDataRequest getUserDataReq = new PlayFab.ServerModels.UpdateUserDataRequest();
-            getUserDataReq.PlayFabId = playerId;
-            getUserDataReq.Data = dataDict;
 
-            var getUserDataT = await PlayFab.PlayFabServerAPI.UpdateUserReadOnlyDataAsync(getUserDataReq);
-            var res = getUserDataT.Result;
+            SetObjectsRequest setObjectsDataReq = new SetObjectsRequest();
+            setObjectsDataReq.Entity = new PlayFab.DataModels.EntityKey();
+            setObjectsDataReq.AuthenticationContext = new PlayFabAuthenticationContext();
+            setObjectsDataReq.AuthenticationContext.EntityToken = getTitleTokenT.Result.EntityToken;
+            setObjectsDataReq.Entity.Id = context.CallerEntityProfile.Entity.Id;
+            setObjectsDataReq.Entity.Type = "title_player_account";
+            setObjectsDataReq.Objects = dataList;
+
+            var setObjectsResT = await PlayFabDataAPI.SetObjectsAsync(setObjectsDataReq);
+            var res = setObjectsResT.Result;
             
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
