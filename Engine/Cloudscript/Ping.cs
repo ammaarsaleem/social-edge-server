6013c851710632617ce420a06f07e43fc648529b
sami-atiq <55493146+sami-atiq@users.noreply.github.com>
2022-05-25 18:18:01 +0500

    Updated ping.cs

--- Engine/Cloudscript/Ping.cs
+++ Engine/Cloudscript/Ping.cs
@@ -1,3 +1,8 @@
+/// @license Propriety <http://license.url>
+/// @copyright Copyright (C) Everplay - All rights reserved
+/// Unauthorized copying of this file, via any medium is strictly prohibited
+/// Proprietary and confidential
+
 using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
